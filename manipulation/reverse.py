e015b6f6aafb5e1bcc5ddc6518616558d6a836e9
votingmanipulation <votingmanipulation@gmail.com>
2017-09-25 11:51:38 +0300

    minor

--- manipulation/reverse.py
+++ manipulation/reverse.py
@@ -19,8 +19,11 @@ def reverse_unweighted(initial_sigmas, alpha, k):
         candidates_sorted = sorted(np.arange(m, dtype=int), key=lambda i: current_awarded[i], reverse=True)
         # now achieve inverse-sort: every candidate gets his rank (high-to-low) in sort
         ballot = np.zeros(m, dtype=int)
-        for rank, cand in enumerate(candidates_sorted):
-            ballot[cand] = rank
+
+        # for rank, cand in enumerate(candidates_sorted):
+        #     ballot[cand] = rank
+
+        ballot[candidates_sorted] = np.arange(m)
 
         ballots.append(ballot)
         current_awarded += alpha[ballot]
